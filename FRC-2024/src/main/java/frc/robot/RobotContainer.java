bbcacd43734a1914d2a3a8fa0e14c61983c79812
Thymos <94299356+TyniusMF@users.noreply.github.com>
2024-02-24 12:21:23 +0100

    Początki do sekwencji strzału
    Potrzeba dużo konfiguracji ale jest jakaś już pod to baza.

--- FRC-2024/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2024/src/main/java/frc/robot/RobotContainer.java
@@ -1,149 +1,251 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.Intake.IntakeNote;
-import frc.robot.commands.Shooter.PassAndShootNote;
-import frc.robot.commands.Swerve.SetZeroHeading;
-import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import java.util.List;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.OIConstants;
-import frc.robot.commands.SwerveJoystickCmd;
-import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.commands.arm.ArmPwmCommand;
+import frc.robot.commands.drive.DriveCommands;
+import frc.robot.commands.drive.FeedForwardCharacterization;
+import frc.robot.commands.intake.IntakeNote;
+import frc.robot.commands.intake.PassNoteToShooter;
+import frc.robot.commands.shooter.StartShooterManual;
+import frc.robot.commands.shooter.StartShooterManualForSeconds;
+import frc.robot.commands.shooter.StartShooterPID;
+import frc.robot.commands.shooter.StopShooterManual;
+import frc.robot.commands.shooter.StopShooterPID;
+import frc.robot.subsystems.ShuffleBoard;
+import frc.robot.subsystems.arm.ArmSubsystem;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIONavx;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOSparkMax;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.limelight.LimelightSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * subsystems, commands, and button mappings) should be declared here.
  */
-
- 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-  public final static ArmSubsystem armSubsystem = new ArmSubsystem();
- public static XboxController driver = new XboxController(PortMap.JOYSTICK.DRIVER_JOYSTICK);
-  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+  // Subsystems
+  private final Drive drive;
+  private final IntakeSubsystem intakeSubsystem;
+  private final ShooterSubsystem shooterSubsystem;
+  private final LimelightSubsystem limelightSubsystem;
+  private final ArmSubsystem armSubsystem;
+  private final ShuffleBoard
+      shuffleBoard; // TODO test if it works in shuffleboard (shuffleboardsubsystem tab)
+  // private final Flywheel flywheel;
 
-  private final Joystick driverController = new Joystick(OIConstants.kDriverControllerPort);
+  // Controller
+  private final CommandXboxController controllerDriver = new CommandXboxController(0);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  //private final CommandXboxController driverController =
-      //new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // Dashboard inputs
+  private final LoggedDashboardChooser<Command> autoChooser;
+  // private final LoggedDashboardNumber flywheelSpeedInput =
+  //     new LoggedDashboardNumber("Flywheel Speed", 1500.0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
-      swerveSubsystem,
-      () -> -driverController.getRawAxis(OIConstants.kDriverYAxis),
-      () -> driverController.getRawAxis(OIConstants.kDriverXAxis),
-      () -> driverController.getRawAxis(OIConstants.kDriverRotAxis),
-      () -> !driverController.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx)));
-    // Configure the trigger bindings
-    configureBindings();
+    switch (Constants.currentMode) {
+      case REAL:
+        // Real robot, instantiate hardware IO implementations
+        drive =
+            new Drive(
+                new GyroIONavx(),
+                new ModuleIOSparkMax(0),
+                new ModuleIOSparkMax(1),
+                new ModuleIOSparkMax(2),
+                new ModuleIOSparkMax(3));
+        intakeSubsystem = new IntakeSubsystem();
+        shooterSubsystem = new ShooterSubsystem();
+        limelightSubsystem = new LimelightSubsystem();
+        armSubsystem = new ArmSubsystem();
+        shuffleBoard = new ShuffleBoard(intakeSubsystem, shooterSubsystem, drive);
+        NamedCommands.registerCommand("IntakeNote", new IntakeNote(intakeSubsystem));
+        NamedCommands.registerCommand("PassNoteToShooter", new PassNoteToShooter(intakeSubsystem));
+        NamedCommands.registerCommand(
+            "RollShooterForSeconds(5)", new StartShooterManualForSeconds(shooterSubsystem, 5));
+        NamedCommands.registerCommand(
+            "StartShooterManual", new StartShooterManual(shooterSubsystem));
+        NamedCommands.registerCommand("StopShooterManual", new StopShooterManual(shooterSubsystem));
+        NamedCommands.registerCommand(
+            "StartShooterPID(1000rpm)", new StartShooterPID(shooterSubsystem, 1000));
+        NamedCommands.registerCommand("StopShooterPID", new StopShooterPID(shooterSubsystem));
+
+        // ! add new subsystems here!
+        // ! add new commands here!
+
+        // flywheel = new Flywheel(new FlywheelIOSparkMax());
+        // drive = new Drive(
+        // new GyroIOPigeon2(),
+        // new ModuleIOTalonFX(0),
+        // new ModuleIOTalonFX(1),
+        // new ModuleIOTalonFX(2),
+        // new ModuleIOTalonFX(3));
+        // flywheel = new Flywheel(new FlywheelIOTalonFX());
+        break;
+
+      case SIM:
+        // Sim robot, instantiate physics sim IO implementations
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim());
+        intakeSubsystem = new IntakeSubsystem();
+        shooterSubsystem = new ShooterSubsystem();
+        limelightSubsystem = new LimelightSubsystem();
+        armSubsystem = new ArmSubsystem();
+        shuffleBoard = new ShuffleBoard(intakeSubsystem, shooterSubsystem, drive);
+        NamedCommands.registerCommand("IntakeNote", new IntakeNote(intakeSubsystem));
+        NamedCommands.registerCommand("PassNoteToShooter", new PassNoteToShooter(intakeSubsystem));
+        NamedCommands.registerCommand(
+            "RollShooterForSeconds(5)", new StartShooterManualForSeconds(shooterSubsystem, 5));
+        NamedCommands.registerCommand(
+            "StartShooterManual", new StartShooterManual(shooterSubsystem));
+        NamedCommands.registerCommand("StopShooterManual", new StopShooterManual(shooterSubsystem));
+        NamedCommands.registerCommand(
+            "StartShooterPID(1000rpm)", new StartShooterPID(shooterSubsystem, 1000));
+        NamedCommands.registerCommand("StopShooterPID", new StopShooterPID(shooterSubsystem));
+        // ! add new subsystems here!
+        // ! add new commands here!
+        // flywheel = new Flywheel(new FlywheelIOSim());
+        break;
+
+      default:
+        // Replayed robot, disable IO implementations
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {});
+        intakeSubsystem = new IntakeSubsystem();
+        shooterSubsystem = new ShooterSubsystem();
+        limelightSubsystem = new LimelightSubsystem();
+        armSubsystem = new ArmSubsystem();
+        shuffleBoard = new ShuffleBoard(intakeSubsystem, shooterSubsystem, drive);
+        NamedCommands.registerCommand("IntakeNote", new IntakeNote(intakeSubsystem));
+        NamedCommands.registerCommand("PassNoteToShooter", new PassNoteToShooter(intakeSubsystem));
+        NamedCommands.registerCommand(
+            "RollShooterForSeconds(5)", new StartShooterManualForSeconds(shooterSubsystem, 5));
+        NamedCommands.registerCommand(
+            "StartShooterManual", new StartShooterManual(shooterSubsystem));
+        NamedCommands.registerCommand("StopShooterManual", new StopShooterManual(shooterSubsystem));
+        NamedCommands.registerCommand(
+            "StartShooterPID(1000rpm)", new StartShooterPID(shooterSubsystem, 1000));
+        NamedCommands.registerCommand("StopShooterPID", new StopShooterPID(shooterSubsystem));
+
+        // ! add new subsystems here!
+        // ! add new commands here!
+        // flywheel = new Flywheel(new FlywheelIO() {});
+        break;
+    }
+
+    // Set up auto routines
+    // NamedCommands.registerCommand(
+    //     "Run Flywheel",
+    //     Commands.startEnd(
+    //             () -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel)
+    //         .withTimeout(5.0));
+    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+
+    // Set up feedforward characterization
+    autoChooser.addOption(
+        "Drive FF Characterization",
+        new FeedForwardCharacterization(
+            drive, drive::runCharacterizationVolts, drive::getCharacterizationVelocity));
+    autoChooser.addDefaultOption(
+        "all-notes - all notes one by one with subwoofer align", new PathPlannerAuto("all-notes"));
+    autoChooser.addOption("rotate-test - 2 circles around", new PathPlannerAuto("rotate-test"));
+    autoChooser.addOption("rotate over notes", new PathPlannerAuto("rotateovernotes"));
+    autoChooser.addOption("fastallnotes", new PathPlannerAuto("fastallnotes"));
+
+    // autoChooser.addOption(
+    //     "Flywheel FF Characterization",
+    //     new FeedForwardCharacterization(
+    //         flywheel, flywheel::runVolts, flywheel::getCharacterizationVelocity));
+
+    // Configure the button bindings
+    configureButtonBindings();
   }
 
-
-private void configureBindings() {
-  
-
-
-   new JoystickButton(driver, Button.kA.value)
-      .onTrue(
-        Commands.runOnce(
-          () -> {
-            armSubsystem.setGoal(Constants.ARM.POSITION.INTAKE);
-            armSubsystem.enable();
-          }
-        )
-      );
-      new JoystickButton(driver, Button.kB.value)
-      .onTrue(
-        Commands.runOnce(
-          () -> {
-            armSubsystem.setGoal(Constants.ARM.POSITION.HORIZONTAL);
-            armSubsystem.enable();
-          }
-        )
-      );
-      new JoystickButton(driver, Button.kY.value)
-      .onTrue(
-        Commands.runOnce(
-          () -> {
-            armSubsystem.setGoal(Constants.ARM.POSITION.SECONDLEVEL);
-            armSubsystem.enable();
-          }
-        )
-      );
-      new JoystickButton(driver, Button.kX.value)
-      .onTrue(
-        Commands.runOnce(
-          () -> {
-            armSubsystem.setGoal(1.4);
-            armSubsystem.enable();
-          }
-        )
-      );
-
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  
-      // driverController.getRawButton(1).whileTrue(new IntakeNote(intakeSubsystem));
-      // driverController.getRawButton(0)
-      // driverController.b().whileTrue(new PassAndShootNote(shooterSubsystem, intakeSubsystem));
-      // driverController.povUp().whileTrue(new SetZeroHeading(swerveSubsystem));
+  private void configureButtonBindings() {
+    drive.setDefaultCommand(
+        DriveCommands.joystickDrive(
+            drive,
+            () -> -controllerDriver.getLeftY(),
+            () -> -controllerDriver.getLeftX(),
+            () -> -controllerDriver.getRightX()));
+    controllerDriver
+        .x()
+        .whileTrue(
+            DriveCommands.angleRotate(
+                drive,
+                () -> -controllerDriver.getLeftY(),
+                () -> -controllerDriver.getLeftX(),
+                limelightSubsystem,
+                limelightSubsystem.getTvInt()));
+    controllerDriver
+        .b()
+        .onTrue(
+            Commands.runOnce(
+                    () ->
+                        drive.setPose(
+                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+                    drive)
+                .ignoringDisable(true));
 
-    new JoystickButton(driverController, 0).whileTrue(new SetZeroHeading(swerveSubsystem));
-    //new JoystickButton(driverJoytick, 2).whenPressed(() -> swerveSubsystem.zeroHeading());
+    // ! ARM AND SHOOTER CONTROLS FOR TESTS
+    controllerDriver.rightBumper().whileTrue(new ArmPwmCommand(armSubsystem, 0.5));
+    controllerDriver.leftBumper().whileTrue(new ArmPwmCommand(armSubsystem, -0.5));
+    // controllerDriver.leftBumper().whileTrue(new StartShooterPID(shooterSubsystem, 1000));
+    // controllerDriver.rightBumper().whileTrue(new StopShooterPID(shooterSubsystem));
 
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    // new Trigger(exampleSubsystem::exampleCondition)
-    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // controller.rightBumper().whileTrue(new IntakeNote(intakeSubsystem));
+    // controller.leftBumper().onTrue(new PassAndShootNote(shooterSubsystem, intakeSubsystem));
 
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    // m_driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand());
+    // controller
+    //     .a()
+    //     .whileTrue(
+    //         Commands.startEnd(
+    //             () -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel));
   }
 
   /**
@@ -152,7 +254,6 @@ private void configureBindings() {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(exampleSubsystem);
+    return autoChooser.get();
   }
 }
