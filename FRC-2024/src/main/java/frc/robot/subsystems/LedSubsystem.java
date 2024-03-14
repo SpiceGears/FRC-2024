e6c8cc80eeaf9c8cbeb3da11bf20d2426739fec6
Bartłomiej Dylewski <71230307+bartekdylewski@users.noreply.github.com>
2024-03-14 09:21:28 +0100

    test

--- FRC-2024/src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ FRC-2024/src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -79,18 +79,7 @@ public class LedSubsystem extends SubsystemBase {
       }
     }
     // TODO LEDs based on auto path
-    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      // Calculate the hue - hue is easier for rainbows because the color
-      // shape is a circle so only one value needs to precess
-      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
-      // Set the value
-
-    }
-
-    // Increase by to make the rainbow "move"
-    m_rainbowFirstPixelHue += 3;
-    // Check bounds
-    m_rainbowFirstPixelHue %= 180;
+    
     // Set the data for this period
     m_led.setLength(m_ledBuffer.getLength());
     m_led.setData(m_ledBuffer);
